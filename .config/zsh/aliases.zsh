cff3557bb620e74ca78001369555beb3c5d6552d
D4ntae <danko.delimar@gmail.com>
2024-02-25 12:58:27 +0100

    zsh customization

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,11 @@
+# Aliases
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias tmux='TERM=xterm-256color tmux'
+alias vim='nvim'
+alias ta='tmux attach -t'
+alias fw_decompress='python3 /home/dantae/repos/siemens_lzp3/fw_decompress.py'
+alias crunch='/home/dantae/repos/crunch-3.6/crunch'
+alias cat='bat'
+
+
---
+++ .config/zsh/envs.zsh
@@ -0,0 +1,5 @@
+export TERM=xterm-256color
+export PATH=$PATH:/home/dantae/scripts:/home/dantae/.local/bin:/home/dantae/links:/home/dantae/go/bin:/home/dantae/.cargo/bin
+export EDITOR=nvim
+
+
